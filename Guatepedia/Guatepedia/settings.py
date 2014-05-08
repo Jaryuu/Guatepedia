c845a0dd31b073f904f962fcd0fe4ccac50fd0ab
Ceborz <richie.zepeda@gmail.com>
2014-05-08 16:32:21 -0600

    Cambio
    Cambié el localhost

--- Guatepedia/Guatepedia/settings.py
+++ Guatepedia/Guatepedia/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'NAME': 'guatepedia',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
